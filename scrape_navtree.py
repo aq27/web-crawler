d9b50213d684b93be6cb5a6b9b344b7d1cf4c42d
aq27 <417551030@qq.com>
2018-01-08 12:58:33 +0800

    BeautifulSoup with nav tree next_siblings

--- scrape_navtree.py
+++ scrape_navtree.py
@@ -6,6 +6,7 @@ import urllib2 as u
 html = u.urlopen("http://www.pythonscraping.com/pages/page3.html")
 bsObj = BeautifulSoup(html, "html.parser")
 
-for child in bsObj.find("table", {"id": "giftList"}).children:
-    print(child)
-
+# for child in bsObj.find("table", {"id": "giftList"}).children: #查找id为giftList的table标签的下一级标签，即tr标签
+#     print(child)
+for sibling in bsObj.find("table", {"id": "giftList"}).tr.next_siblings: #查找id为giftList的table标签的兄弟标签，即除自身之外的tr标签
+    print(sibling)
