6cd89f7d3ebba1e69f868a7106b2478513f0cc50
aq27 <417551030@qq.com>
2018-01-09 10:09:40 +0800

    scrape base on wiki and regex

--- scrape_wiki.py
+++ scrape_wiki.py
@@ -2,8 +2,15 @@
 __author__ = 'lenovo'
 from bs4 import BeautifulSoup
 import urllib2 as u
-html = u.urlopen("http://en.wikipedia.org/wiki/Kevin_Bacon")
+import re
+html = u.urlopen("http://en.wikipedia.org/wiki/Kevin_Bacon") #爬取维基百科网页，与Kevin_Bacon相关联的链接
 bsObj = BeautifulSoup(html, "html.parser")
-for link in bsObj.findAll("a"):
-    if 'href' in link.attrs:
-        print(link.attrs['href'])
+
+# for link in bsObj.findAll("a"): #寻找页面的a链接
+#     if 'href' in link.attrs:
+#         print(link.attrs['href'])
+
+for link in bsObj.find("div", {"id":"bodyContent"}).findAll("a", href=re.compile("^(/wiki/)((?!:).)*$")):
+    if "href" in link.attrs:
+        print(link.attrs["href"])
+        
